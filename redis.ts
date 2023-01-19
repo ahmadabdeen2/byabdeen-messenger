c5f919ba7208f103eb43ad9d89c09d9315ecbe59
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2023-01-19 05:30:33 +0300

    redis url fix

--- redis.ts
+++ redis.ts
@@ -1,5 +1,5 @@
 import Redis from "ioredis";
 
-let client_redis = new Redis(`${process.env.REDIS_URL}`);
+let client_redis = new Redis(`${process.env.NEXT_PUBLIC_REDIS_URL}`);
 
 export default client_redis;
